7c8f3c3318a2f04f0475a35d2024b61d8dd40f7d
Unegbu Kingsley <kingsobino@gmail.com>
2024-07-21 13:32:36 +0100

    fix browser anotation

--- src/assets/nodes/Browser.tsx
+++ src/assets/nodes/Browser.tsx
@@ -1,7 +1,8 @@
-import { Img, Layout, Length, Rect, RectProps, Txt } from "@motion-canvas/2d";
+import { Img, Layout, Length, Node, Rect, RectProps, Txt } from "@motion-canvas/2d";
 import rightIcon from "../images/right.svg";
+import reloadIcon from "../images/refresht.svg";
 import leftIcon from "../images/left.svg";
-import hh from "../../blazer-project/images/screenshot_3.png";
+import { ThreadGenerator, makeRef } from "@motion-canvas/core";
 
 
 export interface BrowserProps extends RectProps {
@@ -11,6 +12,8 @@ export interface BrowserProps extends RectProps {
 }
 
 export class Browser extends Rect {
+  public readonly url: Txt;
+  public readonly page: Rect;
 
   constructor({
     children,
@@ -32,20 +35,32 @@ export class Browser extends Rect {
           <Layout gap={40} padding={20} width="100%" alignItems="center">
             <Img src={leftIcon} width={30} height={30} />
             <Img src={rightIcon} width={30} height={30} />
+            <Img src={reloadIcon} width={30} height={30} />
             <Rect fill="white" zIndex={2} stroke="gray" width="70%" radius={25} padding={10} paddingLeft={30} alignItems="center"> 
-              <Txt fontSize={30}>why me</Txt>
+              <Txt fontSize={30} ref={makeRef(this, "url")}></Txt>
             </Rect>
             
           </Layout>
         </Rect>
-        <Rect grow={1} layout direction="column" fill="red"> 
-          {/* {children} */}
-          <Img src={hh} width="100%" height="100%" />
+        <Rect grow={1} layout direction="column" fill="gray" ref={makeRef(this, "page")}> 
+          {children} 
         </Rect>
       </Rect>
    
     )
   }
 
+
+  
+  * writeUrl(url: string, time: number): ThreadGenerator {
+    yield* this.url.text(url, time)
+  }
+
+  setPage(node: Node) {
+    this.page.removeChildren();
+    this.page.add(
+      node
+    )
+  }
 
 }
