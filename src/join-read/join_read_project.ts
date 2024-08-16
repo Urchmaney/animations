5710be095bb0590a3dbb8f15be4fa44609a611c7
Unegbu Kingsley <kingsobino@gmail.com>
2024-08-16 17:46:46 +0100

    book marketing video

---
+++ src/join-read/join_read_project.ts
@@ -0,0 +1,6 @@
+import { makeProject } from "@motion-canvas/core";
+import scenes from "./scenes";
+
+export default makeProject({
+    scenes
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import motionCanvas from '@motion-canvas/vite-plugin';
 import ffmpeg from '@motion-canvas/ffmpeg';
 
 export default defineConfig({
+  assetsInclude: ['**/*.mov'],
   plugins: [
     motionCanvas({
         project: "./src/**/*_project.ts"
