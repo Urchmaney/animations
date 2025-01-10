e8e26dfe35f98ba93d5f4a2ce3d6c2501a374daa
Unegbu Kingsley <kingsobino@gmail.com>
2025-01-10 17:40:38 +0100

    update blazer codes

--- src/blazer-project/scenes/index.ts
+++ src/blazer-project/scenes/index.ts
@@ -3,5 +3,11 @@ import blazer from "./blazer?scene";
 import setup from "./setup?scene";
 import browser from "./browser?scene";
 import conclusion from "../../linked-list-project/scenes/conclusion?scene"
+import audio from "./audio/blazerAudio.mp3"
+import scenes from "../../africa_path/scenes";
 
-export default [intro, blazer, setup, browser, conclusion];
+export default {
+    scenes: [intro, blazer, setup, browser, conclusion],
+    audio,
+    name: "Blazer Introduction"
+};
